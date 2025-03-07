97f26b4af782c232c516f312643639fc78a1a605
Rahul Kumar <rkrahul1775@gmail.com>
2025-03-07 09:02:45 +0530

    Leetcode 2523. Closest Prime Numbers in Range Solution java

---
+++ src/math/ClosestPrimeNumbersRange.java
@@ -0,0 +1,41 @@
+package math;
+/*TC : O(Rlog(log(R))+R−L)
+SC : O(R)*/
+class ClosestPrimeNumbersRange {
+    public int[] closestPrimes(int left, int right) {
+        boolean[] arr=new boolean[right+1];
+
+        for(int i=0;i<=right;i++)
+            arr[i]=true;
+        arr[0]=false;
+        arr[1]=false;
+        for(int i=2;i*i<=right;i++){
+            if(arr[i]==true){
+                for(int j=i*i;j<=right;j+=i){
+                    arr[j]=false;
+                }
+            }
+        }
+        List<Integer> list=new ArrayList<>();
+        for(int i=left;i<=right;i++){
+            if(arr[i]==true){
+                //System.out.println(i);
+                list.add(i);
+            }
+        }
+
+        //System.out.println(list);
+
+        int min = Integer.MAX_VALUE;
+        int x=-1, y=-1;
+        for(int i=1; i< list.size();i++){
+            if(list.get(i)-list.get(i-1) < min){
+                min = list.get(i)-list.get(i-1);
+                y=list.get(i);
+                x=list.get(i-1);
+            }
+        }
+
+        return new int[]{x, y};
+    }
+}
