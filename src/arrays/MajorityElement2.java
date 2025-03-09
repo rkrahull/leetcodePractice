c38ed414922f06c7ba25ecc9334bc1888cc8a684
Rahul Kumar <rkrahul1775@gmail.com>
2025-03-09 23:26:40 +0530

    Majority Element II | Leetcode 229 Solution Java

--- src/arrays/MajorityElement2.java
+++ src/arrays/MajorityElement2.java
@@ -1,7 +1,7 @@
 package arrays;
-//TC : O(NlogN)
-//SC : O(N)
 class MajorityElement2 {
+    //TC : O(NlogN)
+    //SC : O(N)
     public List<Integer> majorityElement(int[] nums) {
         Map<Integer, Integer> mp=new TreeMap<>();
         for(int val : nums)
@@ -15,5 +15,31 @@ class MajorityElement2 {
         }
         return ans;
 
+    }
+
+    //TC : O(NlogN)
+    //SC : O(1)
+    public List<Integer> majorityElementN(int[] nums) {
+        if(nums.length==1)
+            return new ArrayList<>(Arrays.asList(nums[0]));
+        Arrays.sort(nums);
+        int k=nums.length/3;
+        List<Integer> ans=new ArrayList<>();
+        int i=0,j=1,c=1;
+        while(i<nums.length && j<nums.length){
+            if(nums[i]==nums[j]){
+                c++;
+            }
+            else{
+                if(c>k)
+                    ans.add(nums[i]);
+                c=1;
+                i=j;
+            }
+            j++;
+        }
+        if(c>k)
+            ans.add(nums[j-1]);
+        return ans;
     }
 }
