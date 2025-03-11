2b5b8a0f9e45b1e9b2fd502d884d0137aa59c7e2
Rahul Kumar <rkrahul1775@gmail.com>
2025-03-11 08:35:02 +0530

    Leetcode 1004 Max Consecutive Ones 3 Solution Java

--- src/slidingWindow/MaxConsecutiveOnes3.java
+++ src/slidingWindow/MaxConsecutiveOnes3.java
@@ -1,6 +1,8 @@
 package slidingWindow;
 class MaxConsecutiveOnes3 {
-    public int longestOnes(int[] nums, int k) {
+    //TC : O(N*N)
+    //SC : O(1)
+    public static int longestOnes(int[] nums, int k) {
         int n=nums.length;
         int maxLength=0;
         for(int i=0;i<n;i++){
@@ -14,6 +16,28 @@ class MaxConsecutiveOnes3 {
                 else
                     break;
             }
+        }
+        return maxLength;
+    }
+
+    //TC : O(2N)
+    //SC : O(1)
+    public int longestOnesN(int[] nums, int k) {
+        int n=nums.length;
+        int maxLength=0;
+        int l=0,r=0,zero=0;
+        while(r<n){
+            if(nums[r]==0)
+                zero++;
+            while(zero>k){
+                if(nums[l]==0)
+                    zero--;
+                l++;
+            }
+            if(zero<=k){
+                maxLength=Math.max(r-l+1, maxLength);
+            }
+            r++;
         }
         return maxLength;
     }
