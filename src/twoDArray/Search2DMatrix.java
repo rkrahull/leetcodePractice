682776b2ff7cf65ea3f45aa0a40cfb6023f8e8f9
Rahul Kumar <rkrahul1775@gmail.com>
2025-03-26 22:16:43 +0530

    Leetcode 74 | Search a 2D Matrix Solution Java

---
+++ src/twoDArray/Search2DMatrix.java
@@ -0,0 +1,24 @@
+package twoDArray;
+/*
+TC : O(log(rows*cols))
+Sc : O(1)
+*/
+public class Search2DMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows=matrix.length;
+        int cols=matrix[0].length;
+        int i=0,j=rows*cols-1;
+        while(i<=j){
+            int mid=(i+j)/2;
+            int row=mid/cols;
+            int col=mid%cols;
+            if(matrix[row][col]==target)
+                return true;
+            else if(matrix[row][col]>target)
+                j=mid-1;
+            else
+                i=mid+1;
+        }
+        return false;
+    }
+}
