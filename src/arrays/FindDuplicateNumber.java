76b2f1ade316586d31e0f3411094b504ee988a08
Rahul Kumar <rkrahul1775@gmail.com>
2025-03-28 18:48:11 +0530

    Leetcode 287 | Find the Duplicate Number Solution Java

---
+++ src/arrays/FindDuplicateNumber.java
@@ -0,0 +1,22 @@
+package arrays;
+/*
+TC : O(N)
+SC : O(1)
+*/
+public class FindDuplicateNumber {
+    public int findDuplicate(int[] arr) {
+        int slow=arr[0],fast=arr[0];
+
+        do{
+            slow=arr[slow];
+            fast=arr[arr[fast]];
+        }while(slow != fast);
+
+        fast=arr[0];
+        while(slow != fast){
+            slow=arr[slow];
+            fast=arr[fast];
+        }
+        return slow;
+    }
+}
